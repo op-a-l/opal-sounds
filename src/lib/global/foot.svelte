f149093007f7097bfe9ee5730a1e880f6f3cc9ee
mathis.win11 <opal.elektronaut@gmail.com>
2025-07-13 23:04:39 +0200

    added email at footer

---
+++ src/lib/global/foot.svelte
@@ -0,0 +1,18 @@
+<section>
+    <a href="mailto:opal.elektronaut@gmail.com">
+        opal.elektronaut@gmail.com
+    </a>
+</section>
+
+<style>
+    
+    section{
+        padding: var(--size-5);
+        color: var(--link);
+    }
+
+    a{
+        color: inherit;
+    }
+
+</style>
